b71d7858557c9849a54c4ce7c33375b6f2c81c78
souviknandimodak <souviknandimodak420@gmail.com>
2024-04-21 00:25:41 +0530

    secound change

--- arr.java
+++ arr.java
@@ -3,7 +3,7 @@ class arr {
     public static void main(String[] args) {
         int arr[]={3,1,6,9,4};
         for(int i=0;i<arr.length;i++){
-            if (arr[i]==9){
+            if (arr[i]==1){
                 System.out.println(i);
             }
         }
