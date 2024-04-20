62294d26dc59dc64d98892fec9cf36ea92a99972
souvik nandi modak <souviknandimodak420@gmail.com>
2024-04-20 21:38:39 +0530

    first program in git

---
+++ arr.java
@@ -0,0 +1,12 @@
+
+class arr {
+    public static void main(String[] args) {
+        int arr[]={3,1,6,9,4};
+        for(int i=0;i<arr.length;i++){
+            if (arr[i]==6){
+                System.out.println(i);
+            }
+        }
+        
+    }
+}
