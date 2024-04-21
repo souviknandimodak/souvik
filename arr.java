b74eb42af0f0600c348e90f0e19f1ca9060c747d
souviknandimodak <souviknandimodak420@gmail.com>
2024-04-21 19:06:03 +0530

    this is new number array

--- arr.java
+++ arr.java
@@ -1,7 +1,7 @@
 
 class arr {
     public static void main(String[] args) {
-        int arr[]={3,1,6,9,4};
+        int arr[]={3,1,2,9,4};
         for(int i=0;i<arr.length;i++){
             if (arr[i]==1){
                 System.out.println(i);
